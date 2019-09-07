963c869e68ec00559c414254b40976ca3a1d0281
xyqlx <bgxmy_b@163.com>
2019-09-07 16:07:35 +0800

    添加项目文件。

---
+++ OneNoteTagGUI/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace OneNoteTagGUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PageBrowser pageBrowser;
+        public MainWindow()
+        {
+            this.SizeChanged += MainWindow_SizeChanged;
+            InitializeComponent();
+            InitData();
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.ScrollViewerContent.Height = this.ActualHeight - 260;
+        }
+
+        private void InitData()
+        {
+            pageBrowser = new PageBrowser(Xyqlx.OneNote.App.Notebooks.First(x => x.Name == "介质").Sections.First(x => x.Name == "页面"));
+            this.MainStack.DataContext = pageBrowser;
+            this.IsEnabled = true;
+            this.TextBoxTags.Focus();
+            Task.Run(() => pageBrowser.ClacCommonTags());
+        }
+
+        private void Btn_Next(object sender, RoutedEventArgs e)
+        {
+            pageBrowser.MoveToNext();
+        }
+        private void Btn_Previous(object sender, RoutedEventArgs e)
+        {
+            pageBrowser.MoveToPrevious();
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var listbox = (ListBox)sender;
+            if (listbox.SelectedIndex == -1)
+                return;
+            var tagname = (string)listbox.SelectedValue;
+            pageBrowser.RemoveTag(tagname);
+            this.TextBoxTags.Focus();
+        }
+
+        private void ListBoxCommon_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var listbox = (ListBox)sender;
+            if (listbox.SelectedIndex == -1)
+                return;
+            var tagname = (string)listbox.SelectedValue;
+            pageBrowser.AddTags(new string[] { tagname });
+            this.TextBoxTags.Focus();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    pageBrowser.AddTags(this.TextBoxTags.Text.Split(' ', ';'));
+                    this.TextBoxTags.Text = "";
+                    if (e.KeyboardDevice.Modifiers != ModifierKeys.Control)
+                        pageBrowser.MoveToNext();
+                    break;
+                case Key.Up:
+                    pageBrowser.MoveToPrevious();
+                    break;
+                case Key.Down:
+                    pageBrowser.MoveToNext();
+                    break;
+                case Key.PageUp:
+                    pageBrowser.MoveToTop();
+                    break;
+                case Key.PageDown:
+                    pageBrowser.MoveToBottom();
+                    break;
+            }
+        }
+
+        private void TextBoxPage_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                TextBox textBox = (TextBox)sender;
+                textBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            }
+            else
+            {
+                TextBox_KeyDown(sender, e);
+            }
+        }
+    }
+}
---
+++ OneNoteTagGUI/PageBrowser.cs
@@ -0,0 +1,127 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using one = Xyqlx.OneNote;
+
+namespace OneNoteTagGUI
+{
+    class PageBrowser : INotifyPropertyChanged
+    {
+        private one.Section section;
+        private one.PageInfo[] pageInfos;
+        private one.TagPage current;
+        private ObservableCollection<string> tags;
+        private int index = 0;
+        List<string> commonTags;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //选中无页面的节会出异常（
+        public PageBrowser(one.Section section)
+        {
+            this.section = section;
+            pageInfos = section.PageInfos.ToArray();
+            current = new one.TagPage(one.App.GetPage(pageInfos[0].ID));
+            tags = new ObservableCollection<string>(current.tags);
+            commonTags = new List<string>();
+        }
+
+        public void ClacCommonTags()
+        {
+            var client = new MongoClient("mongodb://127.0.0.1:27017");
+            var database = client.GetDatabase("xyals");
+            var collection = database.GetCollection<BsonDocument>("medium");
+            var query = collection.Find(new BsonDocument()).Project(Builders<BsonDocument>.Projection.Include("tags").Exclude("_id"));
+            var cursor = query.ToCursor();
+            Dictionary<string, int> tagCnt = new Dictionary<string, int>();
+            while(cursor.MoveNext())
+                foreach (var doc in cursor.Current)
+                    foreach (var tag in doc["tags"] as BsonArray)
+                        tagCnt[tag.ToString()] = tagCnt.ContainsKey(tag.ToString()) ? tagCnt[tag.ToString()] + 1 : 0;
+            commonTags = tagCnt.Keys.OrderByDescending(x => tagCnt[x]).Take(20).ToList();
+        }
+
+        public void MoveToPrevious()
+        {
+            if (index != 0)
+                index--;
+            MoveToIndex();
+        }
+        public void MoveToNext()
+        {
+            if (index != pageInfos.Length - 1)
+                index++;
+            MoveToIndex();
+        }
+        public void MoveToTop()
+        {
+            index = 0;
+            MoveToIndex();
+        }
+        public void MoveToBottom()
+        {
+            index = pageInfos.Length - 1;
+            MoveToIndex();
+        }
+
+        public void MoveToIndex()
+        {
+            if (index < 0 || index >= pageInfos.Length)
+                return;
+            current = new one.TagPage(one.App.GetPage(pageInfos[index].ID));
+            tags = new ObservableCollection<string>(current.tags);
+            OnPropertyChanged();
+        }
+
+        protected void OnPropertyChanged(string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void AddTags(IEnumerable<string> other)
+        {
+            current.tags.UnionWith(other.Where(x => x != ""));
+            current.Update();
+            tags.Clear();
+            foreach (var s in current.tags)
+                tags.Add(s);
+            Task.Run(() => UpdateTags(current.Name, current.DateTime, current.tags));
+        }
+        public void RemoveTag(string other)
+        {
+            current.tags.Remove(other);
+            current.Update();
+            tags.Clear();
+            foreach (var s in current.tags)
+                tags.Add(s);
+            Task.Run(() => UpdateTags(current.Name, current.DateTime, current.tags));
+        }
+
+        public void UpdateTags(string name, DateTime dateTime, IEnumerable<string> tags)
+        {
+            //连接数据库
+            var updateBson = new BsonDocument {
+                {"name",name },
+                //{ "time",dateTime },
+                {"tags",new BsonArray(tags) }
+            };
+            var client = new MongoClient("mongodb://127.0.0.1:27017");
+            var database = client.GetDatabase("xyals");
+            var collection = database.GetCollection<BsonDocument>("medium");
+            collection.UpdateOne(Builders<BsonDocument>.Filter.Eq("time", dateTime), new BsonDocument { { "$set", updateBson } }, new UpdateOptions { IsUpsert = true });
+        }
+
+        public string Name => current.Name;
+        public string Content => current.PlainText;
+        public ObservableCollection<string> Tags => tags;
+        public int PageIndex { get => index + 1; set { index = value - 1;MoveToIndex(); } }
+
+        public List<string> CommonTags { get => commonTags; }
+    }
+}
